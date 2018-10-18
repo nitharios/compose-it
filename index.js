59a78d987e04f1d7737f00fffaf04397bf4a523c
Nathan N <Nitharios@users.noreply.github.com>
2018-10-17 18:58:32 -1000

    able to generate help menu

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
+const minimist = require('minimist');
+
 const { dir, files } = require('./lib');
-const [, , ...args] = process.argv;
 
 const run = async () => {
-  const dirName = args[0];
-  const userName = args[1];
+  const args = minimist(process.argv.slice(2))
+  let cmd = args._[0];
+
+  let userName, dirName;
+
+  if (cmd === 'help') {
+    return require('./lib/commands/help')(args);
+  }
+
+  if (args.username || args.u) {
+    userName = args.username ? args.username : args.u;
+  }
+
+  if (args.project || args.d) {
+    dirName = args.project ? args.project : args.d;
+  }
   
   dir.createDirectory(dirName);
   files.createFiles(dirName, userName);
